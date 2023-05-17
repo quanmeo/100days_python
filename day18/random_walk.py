5bffb241241de5ab15f1f0476e24d0106d59321a
Quanmeo <huuquan.learn@gmail.com>
2023-05-17 20:51:02 +0700

    use setheading() instead of right() and refactor codes

--- day18/random_walk.py
+++ day18/random_walk.py
@@ -2,20 +2,15 @@ from random import choice
 from turtle import Turtle, Screen
 colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 
+directions = list(range(0, 360, 90))
+
 mic = Turtle()
 mic.width(5)
 
 for _ in range(200):
     mic.color(choice(colors))
-    direction = choice(list(range(4)))
-    if direction == 0:
-        mic.right(0)
-    elif direction == 1:
-        mic.right(90)
-    elif direction == 2:
-        mic.right(180)
-    else:
-        mic.left(90)
+    direction = choice(directions)
+    mic.setheading(direction)
 
     mic.forward(30)
     
