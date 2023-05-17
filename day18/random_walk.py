65294c86e1f60b88d98c4ef1cb4dffad744affb0
Quanmeo <huuquan.learn@gmail.com>
2023-05-17 21:12:57 +0700

    Updated random_walk using rgb color

--- day18/random_walk.py
+++ day18/random_walk.py
@@ -1,18 +1,27 @@
-from random import choice
-from turtle import Turtle, Screen
-colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+from random import choice, randint
+import turtle as t
+
+def generate_rgb():
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+
+    return (r, g, b)
 
 directions = list(range(0, 360, 90))
 
-mic = Turtle()
+screen = t.Screen()
+mic = t.Turtle()
 mic.width(5)
 
+t.colormode(255)
+
 for _ in range(200):
-    mic.color(choice(colors))
+    mic.color(generate_rgb())
+
     direction = choice(directions)
     mic.setheading(direction)
 
     mic.forward(30)
-    
-screen = Screen()
+
 screen.exitonclick()
